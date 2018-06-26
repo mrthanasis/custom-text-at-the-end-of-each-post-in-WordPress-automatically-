db08e4866511863bc505e9a0b5bed798bad4e2b0
Thanasis Chrysovergis <misterthanasis@gmail.com>
2018-06-26 11:54:01 +0300

    Update function.php

--- function.php
+++ function.php
@@ -1,9 +1,9 @@
-function text_after_content($content) {
-if(!is_feed() && !is_home()) {
-$content.= "<h2>Want more awesome snippets like this one?</h2>";
-$content.= "<p>Don’t forget to check them at  <a href='http://firstsiteguide.com'>WP Loop</a></p>";
-}
-return $content;
-}
+    function text_after_content($content) {
+    if(!is_feed() && !is_home()) {
+    $content.= "<h2>This is title</h2>";
+    $content.= "<p>This is <a href='http://yourlink.com'>text</a></p>";
+    }
+    return $content;
+    }
 
-add_filter ('the_content', 'text_after_content');
+    add_filter ('the_content', 'text_after_content');
